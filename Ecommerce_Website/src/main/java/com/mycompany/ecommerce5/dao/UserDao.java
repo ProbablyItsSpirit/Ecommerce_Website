27732b85a8c4e81808cf5bf70bbc74afcfdf118e
Shardul Dhekane <shardulmd@gmail.com>
2024-04-23 00:31:33 +0530

    Update UserDao.java

--- Ecommerce_Website/src/main/java/com/mycompany/ecommerce5/dao/UserDao.java
+++ Ecommerce_Website/src/main/java/com/mycompany/ecommerce5/dao/UserDao.java
@@ -1,6 +1,9 @@
 
 package com.mycompany.ecommerce5.dao;
 
+import com.mycompany.ecommerce5.entities.user;
+import javax.persistence.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 public class UserDao {
@@ -10,11 +13,17 @@ public class UserDao {
         this.factory = factory;
     }
     //Get user by Email and Password (Method here) (Method will take email and pass and will return user or null
-    public User getuserByEmailAndPassword(String email, String Password){
-        User user = null;
+    public user getuserByEmailAndPassword(String email, String Password){
+        user user = null;
         try {
-            String query = "from User where "
-            
+            String query = "from user where userEmail =: e and userPassword =: p"
+             (Session session = this.factory.openSession()) {
+               Query q = session.createQuery(query);
+               q.setParameter("e",email);
+               q.setParameter('p', password);
+               
+             user =(user)  q.uniqueResult();
+                        }
         } catch (Exception e) {
             e.printStackTrace();
         }
